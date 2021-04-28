13d5bd753e70f8f4a5cb2f53fd691b482ab71dc6
mikechen66 <875661606@qq.com>
2021-04-28 15:35:22 +0800

    Add files via upload

--- test_frcnn_kitti.py
+++ test_frcnn_kitti.py
@@ -4,12 +4,14 @@ import cv2
 import numpy as np
 import pickle
 import time
-# -from keras_frcnn import config
 import tensorflow as tf 
+
+# -from keras_frcnn import config
 from keras_frcnn import config_test
 from keras import backend as K
 from keras.layers import Input
 from keras.models import Model
+
 from keras_frcnn import roi_helpers
 import argparse
 import os
@@ -24,7 +26,7 @@ for gpu in gpus:
 
 
 def format_img_size(img, cfg):
-    """formats the image size based on config"""
+    """Format the image size based on config"""
     img_min_side = float(cfg.im_size)
     (height, width, _) = img.shape
     if width <= height:
@@ -40,7 +42,7 @@ def format_img_size(img, cfg):
 
 
 def format_img_channels(img, cfg):
-    """ formats the image channels based on config """
+    """Format the image channels based on config"""
     img = img[:, :, (2, 1, 0)]
     img = img.astype(np.float32)
     img[:, :, 0] -= cfg.img_channel_mean[0]
@@ -53,13 +55,13 @@ def format_img_channels(img, cfg):
 
 
 def format_img(img, C):
-    """formats an image for model prediction based on config"""
+    """Format an image for model prediction based on config"""
     img, ratio = format_img_size(img, C)
     img = format_img_channels(img, C)
     return img, ratio
 
 
-# Method to transform the coordinates of the bounding box to its original size
+# Define the method to transform the coordinates of the bounding box to its original size
 def get_real_coordinates(ratio, x1, y1, x2, y2):
     real_x1 = int(round(x1 // ratio))
     real_y1 = int(round(y1 // ratio))
@@ -79,27 +81,29 @@ def predict_single_image(img_path, model_rpn, model_classifier_only, cfg, class_
     # -if K.image_dim_ordering() == 'tf':
     if K.image_data_format() == 'channels_last': 
         X = np.transpose(X, (0, 2, 3, 1))
-    # get the feature maps and output from the RPN
+    # Get the feature maps and output from the RPN
     [Y1, Y2, F] = model_rpn.predict(X)
 
-    # this is result contains all boxes, which is [x1, y1, x2, y2]
+    # This is result contains all boxes, which is [x1, y1, x2, y2]
     # -result = roi_helpers.rpn_to_roi(Y1, Y2, cfg, K.image_dim_ordering(), overlap_thresh=0.7)
     result = roi_helpers.rpn_to_roi(Y1, Y2, cfg, K.image_data_format(), overlap_thresh=0.7)
 
-    # convert from (x1,y1,x2,y2) to (x,y,w,h)
+    # Convert from (x1,y1,x2,y2) to (x,y,w,h)
     result[:, 2] -= result[:, 0]
     result[:, 3] -= result[:, 1]
     # -bbox_threshold = 0.8
-    bbox_threshold = 0.10
+    # -bbox_threshold = 0.2
+    bbox_threshold = 0.1
+    
 
-    # apply the spatial pyramid pooling to the proposed regions
+    # Apply the spatial pyramid pooling to the proposed regions
     boxes = dict()
     for jk in range(result.shape[0] // cfg.num_rois + 1):
         rois = np.expand_dims(result[cfg.num_rois * jk:cfg.num_rois * (jk + 1), :], axis=0)
         if rois.shape[1] == 0:
             break
         if jk == result.shape[0] // cfg.num_rois:
-            # pad R
+            # Pad R
             curr_shape = rois.shape
             target_shape = (curr_shape[0], cfg.num_rois, curr_shape[2])
             rois_padded = np.zeros(target_shape).astype(rois.dtype)
@@ -133,7 +137,7 @@ def predict_single_image(img_path, model_rpn, model_classifier_only, cfg, class_
                 [cfg.rpn_stride * x, cfg.rpn_stride * y, cfg.rpn_stride * (x + w), cfg.rpn_stride * (y + h),
                  np.max(p_cls[0, ii, :])])
 
-    # add some nms to reduce many boxes
+    # Add some nms to reduce many boxes
     for cls_num, box in boxes.items():
         boxes_nms = roi_helpers.non_max_suppression_fast(box, overlap_thresh=0.5)
         boxes[cls_num] = boxes_nms
@@ -145,7 +149,7 @@ def predict_single_image(img_path, model_rpn, model_classifier_only, cfg, class_
     img = draw_boxes_and_label_on_image_cv2(img, class_mapping, boxes)
     print('Elapsed time = {}'.format(time.time() - st))
     cv2.imshow('image', img)
-    result_path = './results_images/{}.png'.format(os.path.basename(img_path).split('.')[0])
+    result_path = './result_images/{}.png'.format(os.path.basename(img_path).split('.')[0])
     print('result saved into ', result_path)
     cv2.imwrite(result_path, img)
     cv2.waitKey(0)
@@ -182,14 +186,14 @@ def predict(args_):
     model_classifier_only = Model([feature_map_input, roi_input], classifier)
     model_classifier = Model([feature_map_input, roi_input], classifier)
 
-    # For pretrained one, please put the weights inside model dir. Change cfg.model_path 
-    # in line 173-175 with cfg.base_net_weights, this cfg.base_net_weights 
-    # is pointed to your model that you just downloaded in model dir (by Keras-frcnn config.py).
-    # -print('Loading weights from {}'.format(cfg.model_path))
     print('Loading weights from {}'.format(cfg.base_net_weights))
+    # For a new trained model 
     # -model_rpn.load_weights(cfg.model_path, by_name=True)
+    # For the pretrained resnet50, we set the parameter of cfg.base_net_weights
     model_rpn.load_weights(cfg.base_net_weights, by_name=True)
+    # For a new trained model 
     # -model_classifier.load_weights(cfg.model_path, by_name=True)
+    # For the pretrained ResNet50, we set the parameter of cfg.base_net_weights
     model_classifier.load_weights(cfg.base_net_weights, by_name=True)
 
     model_rpn.compile(optimizer='sgd', loss='mse')
