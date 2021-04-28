56235581162a32b1226e7568cc0227f1a6b44bdf
mikechen66 <875661606@qq.com>
2021-04-28 16:08:32 +0800

    Add files via upload

--- keras_frcnn/visualize.py
+++ keras_frcnn/visualize.py
@@ -1,6 +1,6 @@
 """
-this is common visualize utils to show boxes in detection or tracking,
-this file support both cv2 or PIL library, with separately methods
+This is common visualize utils to show boxes in detection or tracking,
+the file supports both cv2 or PIL library, with separately methods
 """
 import cv2
 import numpy as np
@@ -58,7 +58,7 @@ def _create_unique_color_uchar(tag, hue_step=0.41):
 
 def draw_boxes_and_label_on_image_cv2(img, class_label_map, class_boxes_map):
     """
-    this method using cv2 to show boxes on image with various class labels
+    The method using cv2 to show boxes on image with various class labels
     :param img:
     :param class_label_map: {1: 'Car', 2: 'Pedestrian'}
     :param class_boxes_map: {1: [box1, box2..], 2: [..]}, in every box is [bb_left, bb_top, bb_width, bb_height, prob]
@@ -67,14 +67,13 @@ def draw_boxes_and_label_on_image_cv2(img, class_label_map, class_boxes_map):
     for c, boxes in class_boxes_map.items():
         for box in boxes:
             assert len(box) == 5, 'class_boxes_map every item must be [bb_left, bb_top, bb_width, bb_height, prob]'
-            # checking box order is bb_left, bb_top, bb_width, bb_height
-            # make sure all box should be int for OpenCV
+            # Check box order is bb_left, bb_top, bb_width, bb_height and make sure all box should be int for OpenCV
             bb_left = int(box[0])
             bb_top = int(box[1])
             bb_width = int(box[2])
             bb_height = int(box[3])
 
-            # prob will round 2 digits
+            # Please notice that prob will round 2 digits
             prob = round(box[4], 2)
             unique_color = _create_unique_color_uchar(c)
             cv2.rectangle(img, (bb_left, bb_top), (bb_width, bb_height), unique_color, 2)
@@ -85,7 +84,7 @@ def draw_boxes_and_label_on_image_cv2(img, class_label_map, class_boxes_map):
 
             cv2.rectangle(img, (text_org[0] - 5, text_org[1] + base_line - 5),
                           (text_org[0] + ret_val[0] + 5, text_org[1] - ret_val[1] + 5), unique_color, 2)
-            # this rectangle for fill text rect
+            # The rectangle for fill text rect
             cv2.rectangle(img, (text_org[0] - 5, text_org[1] + base_line - 5),
                           (text_org[0] + ret_val[0] + 5, text_org[1] - ret_val[1] + 5),
                           unique_color, -1)
@@ -95,7 +94,7 @@ def draw_boxes_and_label_on_image_cv2(img, class_label_map, class_boxes_map):
 
 def visualize_det_cv2(img, detections, classes=None, thresh=0.6):
     """
-    visualize detection on image using cv2, this is the standard way to visualize detections
+    Visualize detection on image using cv2, this is the standard way to visualize detections
     :param img:
     :param detections: ssd detections, numpy.array([[id, score, x1, y1, x2, y2]...])
             each row is one object
@@ -135,4 +134,5 @@ def visualize_det_cv2(img, detections, classes=None, thresh=0.6):
                               (text_org[0] + ret_val[0] + 5, text_org[1] - ret_val[1] + 5),
                               unique_color, -1)
                 cv2.putText(img, text_label, text_org, cv2.FONT_HERSHEY_DUPLEX, 0.6, (255, 255, 255), 1)
+                
     return img
